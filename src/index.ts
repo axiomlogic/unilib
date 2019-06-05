7e539d0701dc99221c03a3b6b71ebe74132b9b8b
Nestor Toro <nestor@axiomlogic.com>
2019-06-05 08:49:31 -0700

    Add src/index

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import _UBus from './UBus';
+import _UDispatcher from './UDispatcher';
+import _UError from './UError';
+import _ULogger from './ULogger';
+import _URegistry from './URegistry';
+
+export const UBus = _UBus;
+export const UDispatcher = _UDispatcher;
+export const UError = _UError;
+export const ULogger = _ULogger;
+export const URegistry = _URegistry;
+
+export default {
+  UBus,
+  UDispatcher,
+  UError,
+  ULogger,
+  URegistry
+};
